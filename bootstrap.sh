09486246beb6ca20e27ceb2c4c706b986c2225fb
rayanmargham <rayanmargham4@gmail.com>
2026-08-01 23:13:54 +0100

    missing file bootstrap.sh added

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+drop_into_container() {
+    if ! command -v podman >/dev/null 2>&1 
+    then
+        echo "please install podman, follow your posix-like operating systems docs on how to install podman"
+        exit 1
+    fi
+    podman run --rm -it --name arch -v "$(pwd)":/nyaux:z -w /nyaux archlinux:latest ./make-distro.sh
+}
+drop_into_container
